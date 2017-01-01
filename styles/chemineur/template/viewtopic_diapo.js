7c11606040c66c85727c0a757bc85979e4b8b30c
Dominique Cavailhez <protec@cavailhez.fr>
2017-01-01 16:49:52 +0100

    Commentaire diapo avec simple quote

--- styles/chemineur/template/viewtopic_diapo.js
+++ styles/chemineur/template/viewtopic_diapo.js
@@ -1,21 +1,21 @@
 var l = $(window).width(), h = $(window).height(),
 	u = Math.round (Math.min (l/4, h/3)),
-	d = new Array (['./styles/chemineur/theme/diapo/escape.png', '', '', '']),
-	text = document.getElementsByTagName('var'),
-	t = 0;
-
-<!-- BEGIN postrow -->
-	var comment = text[t++].innerHTML;
-	<!-- IF postrow.S_HAS_ATTACHMENTS -->
-		<!-- BEGIN attachment -->
-			{postrow.attachment.DISPLAY_ATTACHMENT}
-		<!-- END attachment -->
-	<!-- ENDIF -->
-<!-- END postrow -->
+	vars = document.getElementsByTagName('var'),
+	d = new Array (
+		['./styles/chemineur/theme/diapo/escape.png', '', '', '']
+	);
+	
+	for (i = 0; i < vars.length; i++)
+		d.push ([
+			'download/file.php?id='+vars[0].id+'&s='+u*4,
+			'', '',
+			vars[i].parentElement.firstElementChild.innerHTML + // Commentaire du post
+			vars[i].innerHTML // Commentaire de la diapo
+		]);
 
 var mygallery=new simpleGallery({
 	wrapperid: 'diaporama', //ID of main gallery container,
-	title: document.getElementsByTagName('title')[0].text,
+	title: document.getElementsByTagName('title')[0].vars,
 	dimensions: [u*4, u*3], //width/height of gallery in pixels. Should reflect dimensions of the images exactly
 	shift: [(l-u*4)/2, (h-u*3)/2],
 	imagearray: d,

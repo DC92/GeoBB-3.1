513d028f8de979f899302c4c677b2dd3ecc57da4
Dominique Cavailhez <protec@cavailhez.fr>
2017-05-23 12:02:23 +0200

    Redirection points/wri000

--- point/index.php
+++ point/index.php
@@ -1,17 +1,19 @@
 <?php
 
+// Initialisation PhpBB
 define('IN_PHPBB', true);
 $phpbb_root_path = '../';
 $phpEx = substr(strrchr(__FILE__, '.'), 1);
 include($phpbb_root_path . 'common.' . $phpEx);
 
-$req = str_replace ('diapos', 'diapo', $request->server('SCRIPT_URL'));
-$sql = "SELECT topic_id FROM phpbb_posts WHERE post_edit_reason LIKE '%$req'";
+// Recherche dans les points importés de chem2
+$sql = 'SELECT topic_id FROM phpbb_posts WHERE post_edit_reason LIKE \'%'.$request->server('SCRIPT_URL').'\'';
 $result = $db->sql_query_limit($sql, 1);
 $row = $db->sql_fetchrow($result);
 $db->sql_freeresult($result);
 
-if ($row)
-	echo '<meta http-equiv="refresh" content="0; url=http://chemineur.fr/viewtopic.php?t='.$row['topic_id'].'" />';
-else
-	trigger_error('NO_TOPIC');
+// recherche d'un n° de topic
+preg_match ('/\d+$/', $request->server('SCRIPT_URL'), $m);
+
+?>
+<meta http-equiv="refresh" content="0; url=http://chemineur.fr/viewtopic.php?t=<?=$row['topic_id']?:$m[0]?>" />
